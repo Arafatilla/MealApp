7e16b818f8f15523f675fa6b022b593c0a7ca466
Arafatilla <arafatsha1234@gmail.com>
2025-10-30 17:29:53 +0100

    used Positioned widget and enhanced the meals cards

--- MealApp/lib/widgets/meal_item.dart
+++ MealApp/lib/widgets/meal_item.dart
@@ -10,15 +10,56 @@ class MealItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      child: Stack(
-     children: [
-       // if you have a image url from the internt and u want to display it use image.network
+    return Card(
+      margin: EdgeInsets.all(10),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20)
+      ),
+        clipBehavior: Clip.hardEdge,
+       elevation :2 ,
+      child: InkWell(
+        child: Stack(
 
-       FadeInImage(
-           placeholder: MemoryImage(kTransparentImage), image: NetworkImage(meal.imageUrl),
-       ),
-     ],
+       children: [
+         // if you have a image url from the internt and u want to display it use image.network
+
+         FadeInImage(
+             placeholder: MemoryImage(kTransparentImage), image: NetworkImage(meal.imageUrl),
+
+         ),
+         Positioned(
+           bottom: 0,
+           left: 0,
+           right: 0,
+           child: Container(
+             padding: EdgeInsets.symmetric(
+               vertical: 10,
+               horizontal: 10 ,
+             ),
+             color : Colors.black45,
+             child: Column(
+               children: [
+                 Text(meal.title,
+                 textAlign: TextAlign.center,
+                 maxLines: 2,
+                 softWrap: true,
+                 overflow: TextOverflow.ellipsis,
+                 style: TextStyle(
+                   color: Colors.white,
+                   fontSize: 24 ,
+                   fontWeight: FontWeight.bold,
+                 ),),
+
+               ],
+             ),
+           ),
+         ),
+         SizedBox(height:12),
+         Row(
+           children: [],
+         )
+       ],
+        ),
       ),
     );
   }
